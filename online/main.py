61b559e6c98501957c2b19beb68a554246aea881
quimeyv <quimeyv@gmail.com>
2020-02-20 15:23:54 -0300

    Score and dummy solution

--- online/main.py
+++ online/main.py
@@ -1,11 +1,51 @@
-from dataclasses import dataclass
+import sys
 
-def print_data(libraries):
-    # libraries tiene que ser una lista de pares, cada elemento es un par con
+
+class Invalid(Exception):
+    pass
+
+
+def print_data(solution):
+    # solution tiene que ser una lista de pares, cada elemento es un par con
     # el id de la library y una lista de libros a ser escaneados
-    for idx, books in libraries:
+    for idx, books in solution:
         print(idx, len(books))
-        print(' '.join(books))
+        print(' '.join(map(str, books)))
+
+
+def score(D, bs, libraries, solution):
+    result = 0
+    signup = 0
+    scanned = set()
+    for idx, books in solution:
+        t, m, have = libraries[idx]
+        # it takes t days to signup
+        signup += t
+        total = D - signup
+        cnt = 0
+        # process non scanned books in order
+        for book in books:
+            if book not in have:
+                raise Invalid
+            if cnt == m * total:
+                break
+            if book not in scanned:
+                scanned.add(book)
+                result += bs[book]
+                cnt += 1
+    return result
+
+
+def solve(bs, libraries, d):
+    signup = 0
+    solution = []
+    for i, lib in enumerate(libraries):
+        signup += lib[0]
+        lim = lib[1] * (d - signup)
+        solution.append((i, list(lib[2])[:lim]))
+    print(score(d, bs, libraries, solution), file=sys.stderr)
+    return solution
+
 
 def read_data():
     # devuelve una tupla con scores de los libros, descripcion de las libraries
@@ -17,6 +57,13 @@ def read_data():
     libraries = []
     for _ in range(L):
         N, T, M = map(int, input().split())
-        ids = list(map(int, input().split()))
+        ids = set(map(int, input().split()))
         libraries.append((T, M, ids))
     return bs, libraries, D
+
+
+if __name__ == '__main__':
+    bs, libraries, d = read_data()
+    solution = solve(bs, libraries, d)
+    print_data(solution)
+
